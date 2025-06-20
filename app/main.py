3b210902dd0c013aadc1b2d0a166c5bd6fabf88b
Your Name <youremail@example.com>
2025-06-20 11:40:42 +0100

    Added alow origin

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ origins = [
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://vlc-qoi7.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
